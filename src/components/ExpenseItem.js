c055a8c6ca59e57b9817fbe1d6e67cb1aeb5a628
vitamin777 <rlaalsdn8@gmail.com>
2022-06-10 20:29:57 +0900

    Add: css file add commit & component file in attribute apply

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,14 @@
+import './ExpenseItem.css';
+
 function ExpenseItem(){
 	return (
-			<h2>Expense item!</h2>
+			<div className="expense-item">
+				<div>2021 년 3 월 28 일</div>
+				<div className="expense-item__description">
+					<h2>자동차 보험</h2>
+					<div className="expense-item__price">1,430,000 원</div>
+				</div>
+			</div>
 	)
 }
 
