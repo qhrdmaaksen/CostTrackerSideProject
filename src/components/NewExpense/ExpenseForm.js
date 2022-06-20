c9b73ace503c62b1606107f4534f5ba5e70476c9
vitamin777 <rlaalsdn8@gmail.com>
2022-06-20 23:59:20 +0900

    Add: NewExpense folder create, Update: NewExpense,ExpenseForm 작업중 커밋

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ExpenseForm.css'
+import NewExpense from './NewExpense';
+const ExpenseForm = () => {
+	return (
+			<div>
+				<NewExpense />
+			</div>
+	)
+}
+export default ExpenseForm;
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NewExpense.css'
+const NewExpense = () => {
+	return (
+			<div className="new-expense">
+				<from></from>
+			</div>
+	)
+}
+export default NewExpense;
