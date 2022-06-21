2ffdfba4653256a07d56ef56b722f95784d4c55d
vitamin777 <rlaalsdn8@gmail.com>
2022-06-21 23:58:00 +0900

    Update: ExpensesForm handler 작업중 커밋

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ExpenseForm.css'
-import NewExpense from './NewExpense';
 
 const ExpenseForm = () => {
+	const titleChangeHandler = (event) => {
+		console.log(event.target.value);
+	}
+	const [enteredTitle, setEnteredTitle] = useState('');
+	const amountChangeHandler = (event) => {
+		console.log(event.target.value);
+	}
+	const dateChangeHandler = (event) => {
+		console.log(event.target.value)
+	}
 	return (
 			<form action="">
 				<div className="new-expense__controls">
 					<div className="new-expense__control">
 						<label>title</label>
-						<input type="text"/>
+						<input type="text" onChange={titleChangeHandler}/>
 					</div>
 					<div className="new-expense__control">
 						<label>amount</label>
-						<input type="number" min="0" step="10"/>
+						<input type="number" min="0" step="10" onChange={amountChangeHandler}/>
 					</div>
 					<div className="new-expense__control">
 						<label>date</label>
-						<input type="date" min="1950-01-01" max="2022-06-10"/>
+						<input type="date" min="1950-01-01" max="2022-06-10" onChange={dateChangeHandler}/>
 					</div>
 				</div>
 				<div className="new-expense__actions">
