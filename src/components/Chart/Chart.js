c8fc8f5bbe17ee356eba0c1b747e90441b3e88ae
vitamin777 <rlaalsdn8@gmail.com>
2022-07-05 23:23:34 +0900

    AddUpdate: Chart, ChartBar create & 차트 출력 작업중 커밋

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Chart.css'
+import ChartBar from './ChartBar'
+
+const Chart = (props) => {
+	return (
+			<div className="chart">
+				{props.dataPoints.map((dataPoint) => (
+						<ChartBar value={dataPoint.value}
+											maxValue={null}
+											key={dataPoint.label}
+											lebel={dataPoint.label}
+						/>
+				))}
+			</div>
+	)
+}
+export default Chart;
